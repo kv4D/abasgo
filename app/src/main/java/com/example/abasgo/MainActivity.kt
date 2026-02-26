f807c82e2c2af6b1c2ad553bb59c74bcf263b4c9
withered <sergeyzezyanov1@gmail.com>
2026-02-26 17:43:30 +0700

    feat: add new navigation bar
    Updates the way navbar works (like in the prototype)

--- app/src/main/java/com/example/abasgo/MainActivity.kt
+++ app/src/main/java/com/example/abasgo/MainActivity.kt
@@ -4,27 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.rounded.Home
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.abasgo.ui.theme.ABASgoTheme
+import com.example.abasgo.ui.theme.Attention
+import com.example.abasgo.ui.theme.DarkGreen
+import com.example.abasgo.ui.theme.LightGreen
+import com.example.abasgo.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,32 +56,78 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun NavigationBar(
+    current: AppDestinations,
+    onSelect: (AppDestinations) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 20.dp)
+            .padding(12.dp)
+            .clip(RoundedCornerShape(70.dp))
+            .background(DarkGreen)
+            .padding(vertical = 6.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        AppDestinations.entries.forEach { destination ->
+
+            val isSelected = destination == current
+
+            Box(
+                modifier = Modifier
+                    .size(75.dp)
+                    .background(
+                        color = if (isSelected) LightGreen else DarkGreen,
+                        shape = CircleShape
+                    )
+                    .clickable { onSelect(destination) }
+            ) {
+                Column (
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize().fillMaxHeight(),
+
+                ) {
+                    Icon(
+                        painter = painterResource(id = getIconRes(destination.name)),
+                        contentDescription = destination.label,
+                        tint = if (isSelected) Attention else White
+                    )
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Text(
+                        text = destination.label,
+                        textAlign = TextAlign.Center,
+                        color = White,
+                        fontSize = 12.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
 @PreviewScreenSizes
 @Composable
 fun ABASgoApp() {
     var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.MAP) }
 
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            AppDestinations.entries.forEach {
-                item(
-                    icon = {
-                        AppIcon(iconRes = getIconRes(it.name), contentDescription = it.label)
-                    },
-                    label = { Text(it.label) },
-                    selected = it == currentDestination,
-                    onClick = { currentDestination = it }
-                )
-            }
-        }
-    ) {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar(
+                current = currentDestination,
+                onSelect =  {currentDestination = it }
             )
         }
+    ) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
     }
+
 }
 
 enum class AppDestinations(
@@ -71,10 +135,10 @@ enum class AppDestinations(
     val iconName: String,
 ) {
     MAP("Карта", "MAP"),
-    FAVORITE("Избранное", "MAP"),
-    HISTORY("История", "MAP"),
-    ROULETTE("Рулетка", "MAP"),
-    PROFILE("Меню", "MAP"),
+    FAVORITE("Избранное", "FAVORITE"),
+    HISTORY("История", "HISTORY"),
+    ROULETTE("Рулетка", "ROULETTE"),
+    MENU("Меню", "MENU"),
 }
 
 @Composable
--- app/src/main/java/com/example/abasgo/ui/theme/Color.kt
+++ app/src/main/java/com/example/abasgo/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.abasgo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// My colors
+val Attention = Color(0xFFF24C00)
+val White = Color(0xFFFFFBFC)
+val LightGreen = Color(0xFF60993E)
+val DarkGreen = Color(0xFF233329)
+val Black = Color(0xFF191716)
+val BlackTransparent = Color(0xFF1B1C1A)
